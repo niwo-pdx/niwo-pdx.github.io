836cac4e83cfa1ef48eb4aeaec880c247e77f9a4
niwo-pdx <niwo@pdx.edu>
2026-03-17 02:33:19 -0700

    Formatting changes, added carousel next to contact on desktop

---
+++ index.js
@@ -0,0 +1,21 @@
+const track = document.querySelector(".carousel-track");
+const slides = document.querySelectorAll(".slide");
+const nextBtn = document.querySelector(".next");
+const prevBtn = document.querySelector(".prev");
+
+let index = 0;
+
+function updateCarousel() {
+  const width = slides[0].clientWidth;
+  track.style.transform = `translateX(-${index * width}px)`;
+}
+
+nextBtn.addEventListener("click", () => {
+  index = (index + 1) % slides.length;
+  updateCarousel();
+});
+
+prevBtn.addEventListener("click", () => {
+  index = (index - 1 + slides.length) % slides.length;
+  updateCarousel();
+});
